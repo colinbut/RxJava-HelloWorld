bada7d15daac021d6c66dd22d766406704b71b60
Colin But <colinbut@users.noreply.github.com>
2016-11-13 19:27:47 +0000

    implemented code for helloworld example

--- src/main/java/com/mycompany/rx/java/App.java
+++ src/main/java/com/mycompany/rx/java/App.java
@@ -1,10 +1,33 @@
 package com.mycompany.rx.java;
 
+import rx.Observable;
+import rx.functions.Action1;
+
 /**
  * Hello world!
  */
-public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        /** Using 'from' */
+        String[] names = {"Colin", "Michael", "Peter", "Jamie", "Ryan"};
+
+        Observable.from(names).subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println("Hello " + s + "!");
+            }
+        });
+
+
+        /** Using 'just' */
+        Observable.just("Colin","Daniel").subscribe(new Action1<String>() {
+            @Override
+            public void call(String s) {
+                System.out.println("Hello " + s + "!");
+            }
+        });
+
     }
 }
